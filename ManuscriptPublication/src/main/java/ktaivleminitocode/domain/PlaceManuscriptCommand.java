016c9af7a406c3205133624022a07231e7414add
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2025-07-01 11:13:52 +0900

    Fix: type 에러 해결
    정의되지 않은 type 명칭 제거 후 기본 타입으로 변경
    - authorId 제거
    - ManuscriptType 제거

--- ManuscriptPublication/src/main/java/ktaivleminitocode/domain/PlaceManuscriptCommand.java
+++ ManuscriptPublication/src/main/java/ktaivleminitocode/domain/PlaceManuscriptCommand.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class PlaceManuscriptCommand {
 
-    private AuthorId authorId;
+    private Long authorId;
     private String title;
     private String content;
 }
